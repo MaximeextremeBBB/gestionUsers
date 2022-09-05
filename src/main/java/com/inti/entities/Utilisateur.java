73c390eef5c39f6b12504a4df7be36fc0b264d6f
MaximeextremeBBB <maxime.guiraud.1990@gmail.com>
2022-09-05 09:46:15 +0200

    first commit

---
+++ src/main/java/com/inti/entities/Utilisateur.java
@@ -0,0 +1,95 @@
+package com.inti.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Entity
+public class Utilisateur implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	private Long Id;
+	private String nom;
+	private String prenom;
+	@Temporal(TemporalType.DATE)
+	private Date dateNaissance;
+	private int age;
+	private long cin;
+	private long tel;
+	private String numPasseport;
+	public Utilisateur() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Utilisateur(Long id, String nom, String prenom, Date dateNaissance, int age, long cin, long tel,
+			String numPasseport) {
+		super();
+		Id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.dateNaissance = dateNaissance;
+		this.age = age;
+		this.cin = cin;
+		this.tel = tel;
+		this.numPasseport = numPasseport;
+	}
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public long getCin() {
+		return cin;
+	}
+	public void setCin(long cin) {
+		this.cin = cin;
+	}
+	public long getTel() {
+		return tel;
+	}
+	public void setTel(long tel) {
+		this.tel = tel;
+	}
+	public String getNumPasseport() {
+		return numPasseport;
+	}
+	public void setNumPasseport(String numPasseport) {
+		this.numPasseport = numPasseport;
+	}
+	
+	
+	
+	
+}
